d4f0235aa8973b2b7c13e2fe40e9ebb7abae4299
Shorn Jacob <shorn@transparency.consulting>
2019-02-18 16:57:28 +1300

    MAde async await for xmlhttpobject

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-//https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequestEventTarget/onload#Example
+//https://stackoverflow.com/a/53671249/669577
 
-var xmlhttp = new XMLHttpRequest(),
+//Notice no need to create/pass request and resolve. but can be invoked with values for resolving and rejection
+//Returns a promise.
+const xhrRequest = (method,url) => new Promise (  (resolve,reject) => {
+    const xhrObject = new XMLHttpRequest()
+    xhrObject.onload = () => resolve(xhrObject.responseText)
+    xhrObject.onerror = reject
 
-method = 'GET',
+    xhrObject.open(method,url)
+    xhrObject.send()
+}  )
 
-url = 'https://jsonplaceholder.typicode.com/posts/1';
 
+//executes a promise, aasync await is used
+const xhr_get = async (method,url) => {
+    try {
+        let result
+        result = await xhrRequest(method,url)
+        console.log(result)
+    }catch (e) {
+        console.log("Eror")
+    }
+    
+}
 
-xmlhttp.open(method, url, true);
-
-xmlhttp.onload = function () {
-    console.log(xmlhttp.response)
-};
-
-xmlhttp.send();
+xhr_get('GET','https://jsonplaceholder.typicode.com/posts/1')
 
