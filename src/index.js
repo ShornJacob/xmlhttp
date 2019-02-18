a640bd945e79ea5e34e37720b9a4adcb47902012
Shorn Jacob <shorn@transparency.consulting>
2019-02-18 17:33:00 +1300

    MAde notes for promise contructor

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //https://stackoverflow.com/a/53671249/669577
 
-//Notice no need to create/pass request and resolve. but can be invoked with values for resolving and rejection
-//Returns a promise.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises
+//a promise is a returned object to which you attach callbacks, instead of passing callbacks into a function.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#Creating_a_Promise_around_an_old_callback_API
+
 const xhrRequest = (method,url) => new Promise (  (resolve,reject) => {
     const xhrObject = new XMLHttpRequest()
     xhrObject.onload = () => resolve(xhrObject.responseText)
@@ -11,8 +13,13 @@ const xhrRequest = (method,url) => new Promise (  (resolve,reject) => {
     xhrObject.send()
 }  )
 
-
+//The resolve and reject functions, when called, resolve or reject the promise, respectively. 
+//The executor normally initiates some asynchronous work, and then, once that completes, either calls the resolve function
+// to resolve the promise or else rejects it if an error occurred.
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise#Parameters
 //executes a promise, aasync await is used
+
+
 const xhr_get = async (method,url) => {
     try {
         let result
@@ -26,3 +33,6 @@ const xhr_get = async (method,url) => {
 
 xhr_get('GET','https://jsonplaceholder.typicode.com/posts/1')
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Using_promises#Creating_a_Promise_around_an_old_callback_API
+//In an ideal world, all asynchronous functions would already return promises.
+//Basically, the promise constructor takes an executor function that lets us resolve or reject a promise manually.
