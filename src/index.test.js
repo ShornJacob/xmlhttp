82ecf932d63986c6195a088119f490f0221d2f7c
Shorn Jacob <shorn@transparency.consulting>
2019-02-19 13:30:32 +1300

    MAde a mock fuction rather than mocking a module to test XMLHttp

--- src/index.test.js
+++ src/index.test.js
@@ -1,21 +1,13 @@
-import { XMLHttp } from "./XMLHttp";
-
-jest.mock('./XMLHttp')
-
 //https://jestjs.io/docs/en/asynchronous#promises
 //testing an async - using real api
-test('testing an async', () => {
+test('testing a mock function', () => {
     const method = 'GET'
     const url = 'https://jsonplaceholder.typicode.com/posts/1'
 
-    //if you omit this return statement, your test will complete before the promise returned
-    return XMLHttp(method,url).then(data => {
-        expect(data).toEqual({
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-          })
-    })
+    const XMLHTTP = jest.fn()
 
-  });
+    XMLHTTP(method,url)
+
+    expect(XMLHTTP.mock.calls.length).toBe(1)
+
+})
